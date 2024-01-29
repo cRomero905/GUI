9a37bcb85b10cf0318495716d4c5f14816627070
christian.romero24 <christian.romero24@d155.org>
2024-01-29 08:34:27 -0600

    added flag images

---
+++ src/Main.java
@@ -0,0 +1,42 @@
+import javax.swing.*;
+import java.awt.*;
+
+
+public class Main {
+    public static void main(String[] args) {
+        JFrame frame = new JFrame("Guess the flag");
+        frame.setSize(1100, 600);
+
+
+        JPanel panel = new JPanel();
+        panel.setLayout(new BorderLayout());
+        JPanel subPanelImages = new JPanel();
+
+        JLabel label = new JLabel("Pick Level");
+
+        JLabel img = new JLabel();
+        img.setIcon(new ImageIcon("images/Flags/US.png"));
+        JPanel imgPanel = new JPanel();
+
+        JButton button1 = new JButton("Easy");
+        JButton button2 = new JButton("Medium");
+        JButton button3 = new JButton("Hard");
+        JTextField textField = new JTextField("Enter guess", 10);
+
+
+        panel.add(label, BorderLayout.NORTH);
+        subPanelImages.add(button1);
+        subPanelImages.add(button2);
+        subPanelImages.add(button3);
+        imgPanel.add(img);
+        panel.add(subPanelImages, BorderLayout.WEST);
+        panel.add(textField, BorderLayout.SOUTH);
+        panel.add(imgPanel, BorderLayout.CENTER);
+
+
+        frame.add(panel);
+        frame.setVisible(true);
+
+
+    }
+}
